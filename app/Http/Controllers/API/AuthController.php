10892e31b570974a974b845119d68d9c06979f4a
fuzail14 <fuzailrajpoot1466@gmail.com>
2022-09-05 17:39:35 +0500

    cnicloginsignupapi

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+
+use App\Http\Controllers\Controller;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\HasApiTokens;
+
+
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validations = $request->validate([
+
+            'firstname' => 'required|string|max:191',
+            'lastname' => 'required|string|max:191',
+            'cnic' => 'required|string|max:191|unique:users,cnic',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::create([
+
+            'firstname' => $validations['firstname'],
+            'lastname' => $validations['lastname'],
+            'cnic' => $validations['cnic'],
+            'password' => Hash::make($validations['password']),
+
+
+        ]);
+
+        $token = $user->createToken('MainAdmin')->plainTextToken;
+        $response = [
+
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 200);
+    }
+
+    public function logout()
+    {
+
+
+        auth()->user()->tokens->each(function ($token, $key) {
+            $token->delete();
+        });
+
+
+        return response(['message' => 'Logged out Successfully']);
+    }
+
+    public function alluser()
+    {
+
+        $user = User::all();
+
+        return response($user);
+
+    }
+
+    public function login(Request $request)
+    {
+
+        $loginvalidations = $request->validate([
+
+
+            'cnic' => 'required|string|max:191',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('cnic', $loginvalidations['cnic'])->first();
+
+        if(!$user || !Hash::check($loginvalidations['password'],$user->password))
+        {
+
+            return response(['message'=> 'Invalid creditanls'],401);
+        }
+        else
+        {
+            $token = $user->createToken('MainAdmin')->plainTextToken;
+
+            $response = [
+
+                'user' => $user,
+                'token' => $token,
+            ];
+
+            return response($response,200);
+
+
+
+        }
+    }
+
+
+   
+}
